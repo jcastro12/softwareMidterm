d160059a03a51cc830262d9a1baf8568167619c1
Joshua C <113030588+jcastro12@users.noreply.github.com>
2025-05-05 03:18:11 -0500

    Delete softwareFinalReal directory

--- softwareFinalReal/src/main/java/database/DBService.java
+++
@@ -1,9 +0,0 @@
-//interface for different types of databases
-package database;
-
-import java.util.List;
-
-public interface DBService
-{
-    List<User> loadUsers(); // Method to load users from a database or file
-}
